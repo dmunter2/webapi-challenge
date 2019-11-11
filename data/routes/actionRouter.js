ba39cccc3a8e3944270b11985716a3bd473cf2a3
dmunter2 <49967133+dmunter2@users.noreply.github.com>
2019-11-10 17:47:01 -0800

    action crud added

--- data/routes/actionRouter.js
+++ data/routes/actionRouter.js
@@ -1,6 +1,7 @@
 const express = require('express');
 
 const router = express.Router()
+
 const actions = require('../helpers/actionModel')
 
 
@@ -12,6 +13,32 @@ router.get('/', (req, res) => {
     .catch(err => {
         res.status(500).json({error: "did not work"})
     })
+})
+
+router.get('/:id', (req,res) => {
+    const actionID = req.params.id;
+    actions.get(actionID)
+        .then(id => {
+            res.status(201).json(id)
+        })
+})
+
+
+
+
+router.post('/', (req,res) => {
+    const actionInfo = req.body;2
+    if(req.body.notes && req.body.description){
+        actions.insert(actionInfo)
+            .then(action => {
+                res.status(201).json({success: true})
+            })
+            .catch(err => {
+                res.status(500).json({success: "there was an error"})
+            })
+    } else {
+        res.status(500).json({message: "incorrect parametser"})
+    }
 })
 
 
